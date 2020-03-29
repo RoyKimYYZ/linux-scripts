9a18bace912ea14e31b474d1c6c23bc3e790fdc1
Roy Kim <roy@roykim.ca>
2020-03-28 23:25:33 -0400

    update

--- setup-containers-tools.sh
+++ setup-containers-tools.sh
@@ -35,3 +35,5 @@ source /usr/share/bash-completion/bash_completion
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 kubectl completion bash >/etc/bash_completion.d/kubectl
 echo 'alias k=kubectl' >>~/.bashrc # set alias
+
+  
