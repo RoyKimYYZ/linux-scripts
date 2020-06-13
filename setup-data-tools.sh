a42f719ee0c08b53bec6b91642b5fb5582ed3c3d
Roy Kim <roy@roykim.ca>
2020-06-12 22:02:50 -0400

    wordpress stuff

--- setup-data-tools.sh
+++ setup-data-tools.sh
@@ -2,3 +2,10 @@
 sudo app-get update
 sudo apt-get install mysql-client
 mysql -h HOST -P PORT_NUMBER -u USERNAME -p
+
+# Sql Package
+# https://docs.microsoft.com/en-us/sql/tools/sqlpackage-download?view=sql-server-ver15#get-sqlpackage-net-core-for-linux
+sudo apt-get install libunwind8
+lsb_release -a
+# install the libicu library based on the Ubuntu version
+sudo apt-get install libicu60      # for 18.x
